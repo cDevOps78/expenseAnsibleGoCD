84f644ba42f5efed1476135e4464512bfd97def9
chaitu1 <chaitu143@gmail.com>
2024-03-23 23:09:32 +0530

    adjusting ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 
-ansible-playbook -i ${component}-dev.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${component}
+ansible-playbook -i ${component}-dev.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${component} rolecall.yaml

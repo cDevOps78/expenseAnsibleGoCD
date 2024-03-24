a76b2e45a381b44b1d8321801feeeea31dbffe25
chaitu1 <chaitu143@gmail.com>
2024-03-24 14:33:19 +0530

    small correction {{ env }} t0 $env

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,4 @@ component=$1
 echo ${component}
 
 echo ${env}
-# ansible-playbook -i ${component}-{{ env }}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e role_name=${component} rolecall.yaml
+# ansible-playbook -i ${component}-${env}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e role_name=${component} rolecall.yaml

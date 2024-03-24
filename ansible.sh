265219ef2f3906780dfd6c7faa18570a60b47622
chaitu1 <chaitu143@gmail.com>
2024-03-24 14:26:02 +0530

    added environment varibale env: dev

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,6 @@
 component=$1
 
-ansible-playbook -i ${component}-{{ env }}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e role_name=${component} rolecall.yaml
+echo ${component}
+
+echo ${env}
+# ansible-playbook -i ${component}-{{ env }}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e role_name=${component} rolecall.yaml

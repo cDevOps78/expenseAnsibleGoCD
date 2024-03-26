76d1bd84da2371b070630245ce29570a368f68ef
chaitu1 <chaitu143@gmail.com>
2024-03-26 21:10:36 +0530

    practing provisioner part-2

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 # we are passing ${env} variable  value from gocd pipelines.
-ansible-playbook -i ${component}-${env}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e component=${component-e} -e role_name=${component} rolecall.yaml
+ansible-playbook -i ${component}-${env}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e component=${component} -e role_name=${component} rolecall.yaml
 
 #-Mysq1-#
 # ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=dev -e role_name=mysql -e component=mysql rolecall.yaml

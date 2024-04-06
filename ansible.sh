de7f3fa4c3ec4208bce2f19e205e75ce417fe60e
chaitu1 <chaitu143@gmail.com>
2024-04-06 11:49:17 +0530

    vault setup for mysql

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,9 @@
 component=$1
 # we are passing ${env} variable  value from gocd pipelines.
-ansible-playbook -i ${component}-${env}.azcart.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${env} -e component=${component} -e role_name=${component} rolecall.yaml
+ansible-playbook get-vault-secrets.yaml -e env=${env} -e component=${component} -e vault_token=${vault_token}
+
+
+ansible-playbook -i ${component}-${env}.azcart.online, -e "@common-secrets.json" -e "@${component}-secrets.json" -e env=${env} -e component=${component} -e role_name=${component} rolecall.yaml
 
 #-Mysq1-#
 # ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=dev -e role_name=mysql -e component=mysql rolecall.yaml

b2e9c28ded83bfcac2849d12610e2a6f80d22853
chaitu1 <chaitu143@gmail.com>
2024-04-06 11:49:44 +0530

    vault setup for mysql

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@ component=$1
 ansible-playbook get-vault-secrets.yaml -e env=${env} -e component=${component} -e vault_token=${vault_token}
 
 
-ansible-playbook -i ${component}-${env}.azcart.online, -e "@common-secrets.json" -e "@${component}-secrets.json" -e env=${env} -e component=${component} -e role_name=${component} rolecall.yaml
+# ansible-playbook -i ${component}-${env}.azcart.online, -e "@common-secrets.json" -e "@${component}-secrets.json" -e env=${env} -e component=${component} -e role_name=${component} rolecall.yaml
 
 #-Mysq1-#
 # ansible-pull -i localhost, -U https://github.com/cDevOps78/expenseAnsibleGoCD -e env=dev -e role_name=mysql -e component=mysql rolecall.yaml
